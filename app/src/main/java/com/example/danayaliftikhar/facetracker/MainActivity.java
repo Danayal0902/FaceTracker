19c82df8843b8d071bf3f81d05f5a4ea5877edb7
Danayal Iftikhar <danayal_94@hotmail.com>
2018-04-09 01:30:53 +0100

    Camera function

--- app/src/main/java/com/example/danayaliftikhar/facetracker/MainActivity.java
+++ app/src/main/java/com/example/danayaliftikhar/facetracker/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.danayaliftikhar.facetracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
-}
+        Button b = (Button)findViewById(R.id.btnStart);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.VIDEO_CAPTURE");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+    }
